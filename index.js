f7a519711d04fdbd925bf767cab2b80cb3ba6b27
Amanda LeMoine <amanda1@mnlemoines.com>
2020-12-05 15:01:52 -0800

    commits added to the index.js file.

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ let employeeArray = [];
 
 // array of questions for user
 
-
+// Initial question to ask of what employee type is the user collection data on
 const employeeQuestion = [
   {
     type: "list",
@@ -20,64 +20,69 @@ const employeeQuestion = [
   },
 ];
 
-
-
+// asks the question declared above
 function askQuestions() {
   inquirer.prompt(employeeQuestion).then((response) => {
-    // console.log(response.employeeType);
+    // after the response is collected then store the employee type in the employee variable
     employee = response.employeeType;
 
+    // using the employee variable to personalize the potential next questions to ask
     const standardQuestions = [
-        {
-          type: "input",
-          name: "name",
-          message: "What is the "+employee+"'s name?",
-        },
-        {
-          type: "input",
-          name: "id",
-          message: "What is the "+employee+"'s id?",
-        },
-        {
-          type: "input",
-          name: "email",
-          message: "What is the "+employee+"'s email?",
-        },
-      ];
+      {
+        type: "input",
+        name: "name",
+        message: "What is the " + employee + "'s name?",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "What is the " + employee + "'s id?",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is the " + employee + "'s email?",
+      },
+    ];
 
+    // This is the manager specific question, the standard questions come first
     const managerQuestions = [
-        ...standardQuestions,
-        {
-          type: "input",
-          name: "officeNumber",
-          message: "What is the "+employee+"'s office number?",
-        },
-      ];
-      
-      const internQuestions = [
-        ...standardQuestions,
-        {
-          type: "input",
-          name: "school",
-          message: "What is the "+employee+"'s school?",
-        },
-      ];
-      
-      const engineerQuestions = [
-        ...standardQuestions,
-        {
-          type: "input",
-          name: "gitHub",
-          message: "What is the "+employee+"'s GitHub username?",
-        },
-      ];
+      ...standardQuestions,
+      {
+        type: "input",
+        name: "officeNumber",
+        message: "What is the " + employee + "'s office number?",
+      },
+    ];
 
+    // This is the intern specific question, the standard questions come first
+    const internQuestions = [
+      ...standardQuestions,
+      {
+        type: "input",
+        name: "school",
+        message: "What is the " + employee + "'s school?",
+      },
+    ];
+
+    //this is the engineer specific question, the standard questions come first.
+    const engineerQuestions = [
+      ...standardQuestions,
+      {
+        type: "input",
+        name: "gitHub",
+        message: "What is the " + employee + "'s GitHub username?",
+      },
+    ];
+
+    // Depending on the employee selected by the user the employee specific questions are asked
     switch (employee) {
       case "Manager":
+        // if manager is selected, then it is removed from the initial question array. Only one manager can be selected
         employeeQuestion[0].choices.splice(0, 1);
-        inquirer.prompt(managerQuestions).then((managerResponse) => {
-        //   console.log(managerResponse);
 
+        // Ask the manager specific questions
+        inquirer.prompt(managerQuestions).then((managerResponse) => {
           const manager = new Manager(
             managerResponse.name,
             managerResponse.id,
@@ -85,27 +90,33 @@ function askQuestions() {
             managerResponse.officeNumber
           );
 
+          // add the manager object instance to the employee array to use later for generating the HTML file
           employeeArray.push(manager);
+          // ask the user if they have reached the end of their employees
           endOfEmployees();
         });
         break;
       case "Engineer":
+
+        // Ask the engineer specific questions
         inquirer.prompt(engineerQuestions).then((engineerResponse) => {
-        //   console.log(engineerResponse);
           const engineer = new Engineer(
             engineerResponse.name,
             engineerResponse.id,
             engineerResponse.email,
             engineerResponse.gitHub
           );
-
+          
+          // add the engineer object instance to the employee array to use later for generating the HTML file
           employeeArray.push(engineer);
+          // ask the user if they have reached the end of their employees
           endOfEmployees();
         });
         break;
       case "Intern":
+
+        // ask the intern specific questions
         inquirer.prompt(internQuestions).then((internResponse) => {
-        //   console.log(internResponse);
           const intern = new Intern(
             internResponse.name,
             internResponse.id,
@@ -113,7 +124,9 @@ function askQuestions() {
             internResponse.officeNumber
           );
 
+          // add the intern object instance to the employee array to use later for generating the HTML file
           employeeArray.push(intern);
+          // ask the user if they have reached the end of their employees
           endOfEmployees();
         });
         break;
@@ -122,20 +135,23 @@ function askQuestions() {
 }
 
 function endOfEmployees() {
+  // ask the user if they have more employees to enter information
   inquirer
     .prompt([
       {
         type: "list",
         name: "end",
-        message: "Are there more Employees to Enter>",
+        message: "Are there more Employees to Enter?",
         choices: ["Yes", "No"],
       },
     ])
     .then((response) => {
-    //   console.log(response);
+      // if the user responded yes then askQuestions is called again to ask what employee type they are entering
       if (response.end == "Yes") {
         askQuestions();
-      } else {
+      } else 
+      // if the user responded no then a HTML file is generated using the employeeArray
+      {
         fs.writeFile("./output/index.html", render(employeeArray), (err) =>
           err ? console.error(err) : console.log("Index.html is saved")
         );
